a60c26e0a006a655e4d3a6ec36fc5c3238bfd0f5
Sam van der Poel <samrvanderpoel@gmail.com>
2022-01-16 23:01:14 -0500

    edit to randomly sample point set instance

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import os
+import numpy as np
 from bitonic import *
 
-coords = [[0, 0], [4, 3], [5, -5], [6, -4], [7, -5], [8, -4], [9, 3], [13, 6], [17, 0]]
+def pts_uni(numpts, xlim=[0,1], ylim=[0,1]):
+    xs = xlim[0] + abs(xlim[1]-xlim[0])*np.random.random(numpts)
+    ys = ylim[0] + abs(ylim[1]-ylim[0])*np.random.random(numpts)
+    return np.asarray(list(zip(xs,ys)))
+
+numpts = 250
+coords = sorted(pts_uni(numpts), key=lambda k: [k[0], k[1]])
 edges = get_bitonic_edges(coords, 0)
 print(edges)
