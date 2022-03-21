5f919508066672d2c177d8902875ba8c81ce3207
Sam van der Poel <samrvanderpoel@gmail.com>
2022-03-21 17:38:23 -0400

    fix method for sorting coordinates prior to main fnc call

--- bitonic.py
+++ bitonic.py
@@ -4,32 +4,41 @@ import numpy as np
 import numpy.ctypeslib as ctl
 import os
 
-def get_bitonic_edges(coords, k):
+def get_bitonic_edges(coords, k=0):
     n = len(coords)
     coords = [[x,y] for x, y in coords]
-    os.system("python bitonic.py --coords \"" \
-              + str(coords) + "\" > res" + str(k) + ".txt")
-    lines = open("res" + str(k) + ".txt", "r").readlines()
-    os.remove("res" + str(k) + ".txt")
-    edges = [(int(x), int(y))
-             for x, y in [line.strip().split('\t')
-             for line in lines]]
-    return edges + [(n-1, n-2)]
+    os.system(
+        f"python bitonic.py --coords \"{coords}\" > res{k}.txt"
+    )
+    lines = open(f"res{k}.txt", "r").readlines()
+    os.remove(f"res{k}.txt")
+    edges = [
+        (int(x), int(y))
+        for x, y in [
+            line.strip().split('\t')
+            for line in lines
+        ]
+    ]
+    return edges + [(n-2, n-1)]
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--coords', type=str, required=True)
-    args = parser.parse_args()
-    coords = eval(args.coords)
-    coords = [c for pair in coords for c in pair]
+    coords_input = eval(parser.parse_args().coords)
+    coords_sort = sorted(coords_input, key=lambda k: [k[0], k[1]])
+    coords_flat = [c for pair in coords_sort for c in pair]
 
     libname = 'bitolib.so'
     libdir = './'
     lib = ctl.load_library(libname, libdir)
 
     py_bitonic = lib.bitonic
-    py_bitonic.argtypes = [ctl.ndpointer(np.float64, 
-                                         flags='aligned, c_contiguous'), 
-                           ctypes.c_int]
-    A = np.array(coords, dtype=np.float64)
-    py_bitonic(A, int(len(coords)/2))
+    py_bitonic.argtypes = [
+        ctl.ndpointer(
+            np.float64,
+            flags='aligned, c_contiguous'
+        ), 
+        ctypes.c_int
+    ]
+    A = np.array(coords_flat, dtype=np.float64)
+    py_bitonic(A, int(len(coords_flat)/2))
--- main.py
+++ main.py
@@ -2,12 +2,13 @@ import os
 import numpy as np
 from bitonic import *
 
-def pts_uni(numpts, xlim=[0,1], ylim=[0,1]):
-    xs = xlim[0] + abs(xlim[1]-xlim[0])*np.random.random(numpts)
-    ys = ylim[0] + abs(ylim[1]-ylim[0])*np.random.random(numpts)
+def uniform_square(numpts, xlim=[0,1], ylim=[0,1]):
+    xs = xlim[0] + abs(xlim[1]-xlim[0]) * np.random.random(numpts)
+    ys = ylim[0] + abs(ylim[1]-ylim[0]) * np.random.random(numpts)
     return np.asarray(list(zip(xs,ys)))
 
-numpts = 250
-coords = sorted(pts_uni(numpts), key=lambda k: [k[0], k[1]])
-edges = get_bitonic_edges(coords, 0)
-print(edges)
+if __name__ == "__main__":
+    numpts = 250
+    coords = uniform_square(numpts)
+    edges = get_bitonic_edges(coords)
+    print(edges)
